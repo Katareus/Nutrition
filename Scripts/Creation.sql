6f0ee13679da7538a2118adb98f755e7698cd4cc
Alex <alex92caa@gmail.com>
2022-01-28 10:57:57 +0100

    Adding table creation sript

---
+++ Scripts/Creation.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Categories]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL
+);
+
+CREATE TABLE [dbo].[Ingredients]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL,
+	[CategoryId] int FOREIGN KEY REFERENCES Categories(Id) NOT NULL
+);
+
+CREATE TABLE [dbo].[Synonyms]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL,
+	[IngredientId] int FOREIGN KEY REFERENCES Ingredients(Id) NOT NULL
+);
+
